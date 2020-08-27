120909c7b41a83d300a0030cddd2f41b562b3a19
Keld Graugaard <keld@graugaard.org>
2020-08-27 11:20:38 +0200

    First Commit

---
+++ app/src/main/java/org/graugaard/androidsample/MainActivity.kt
@@ -0,0 +1,25 @@
+package org.graugaard.androidsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun convertCurrency(view: View){
+        if (dollarText.text.isNotEmpty()){
+            val dollarValue = dollarText.text.toString().toFloat()
+            val euroValue = dollarValue * .85f
+
+            textView.text = euroValue.toString()
+        } else {
+            textView.text = getString(R.string.no_value_string)
+        }
+    }
+}
